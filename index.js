4f1de597e5d686461ad4b97d1141f1753d96a942
山田謙士郎 <yamadakenshirou@yamadakenshirounoMacBook-Air.local>
2024-03-04 17:58:35 +0900

    dentaku

--- index.js
+++ index.js
@@ -1,33 +1,50 @@
-const operatorReg = /^(\+|-|\*|\/)$/;
-const numReg = /^([0-9]|00)$/;
-  
-let canAddDecimalPoint = true;
-
 function clickButton(btn) {
-   const preValue = document.calculator.display.value.slice(-1);
-   let value = btn.value;
+    const preValue = document.calculator.display.value.slice(-1);
+    let value = btn.value;
 
-   if (value == ".") {
-      if (canAddDecimalPoint === false) return;
-      if (preValue === "" || preValue === "." || operatorReg.test(preValue)) {
-         return;
-      }
-      canAddDecimalPoint = false;
-   }
-   else if (numReg.test(value) === false) {
-      canAddDecimalPoint = true;
-   }
-   if (operatorReg.test(value) && operatorReg.test(preValue)) {
-      return; 
-   }
-   if (value == "00" && /^([0-9]|\.)$/.test(preValue) === false) {
-      value = "0"
-   }
-   if (value === "=") {
-      document.calculator.display.value = eval(document.calculator.display.value);
-   } else if (value === "AC") {
-      document.calculator.display.value = "";
-   } else {
-      document.calculator.display.value += value;
-   }
+    if (value == ".") {
+        if (preValue === "" || preValue === "." || preValue === "+" || preValue === "-" || preValue === "*" || preValue === "/") {
+           return;
+        } 
+    }
+    if (value == "+") {
+        if (preValue === "" || preValue === "." || preValue === "+" || preValue === "-" || preValue === "*" || preValue === "/") {
+           return;
+        } 
+    }
+    if (value == "-") {
+        if (preValue === "" || preValue === "." || preValue === "+" || preValue === "-" || preValue === "*" || preValue === "/") {
+           return;
+        } 
+    }
+    if (value == "*") {
+        if (preValue === "" || preValue === "." || preValue === "+" || preValue === "-" || preValue === "*" || preValue === "/") {
+           return;
+        } 
+    }
+    if (value == "/") {
+        if (preValue === "" || preValue === "." || preValue === "+" || preValue === "-" || preValue === "*" || preValue === "/") {
+           return;
+        } 
+    }
+
+    if (value == "0") {
+        if (preValue === "" || preValue === "+" || preValue === "-" || preValue === "*" || preValue === "/") {
+            return;
+        }
+    }
+
+    if (value == "00") {
+        if (preValue === "" || preValue === "+" || preValue === "-" || preValue === "*" || preValue === "/") {
+            return;
+        }
+    }
+
+    if (value === "=") {
+        document.calculator.display.value = eval(document.calculator.display.value);
+    } else if (value === "AC") {
+        document.calculator.display.value = "";
+    } else {
+        document.calculator.display.value += value;
+    }
 }
